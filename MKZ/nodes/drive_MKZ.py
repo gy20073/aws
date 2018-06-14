d28934c029b79bc052e1f903605a786703d2a434
Yang Gao <gy20073@gmail.com>
2018-06-13 21:07:18 -0700

    prune the original driving model specific parts

--- MKZ/nodes/drive_MKZ.py
+++ MKZ/nodes/drive_MKZ.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# This file put (1)driving model, (2)keyboard input (3)publish twist command (4)receive image together
+
 # roslib related
 import roslib
 roslib.load_manifest('dbw_mkz_msgs')
@@ -17,22 +19,14 @@ import sys, os, time
 # some constants
 global PREDICTION_HZ, STOP_FUTURE_FRAMES
 PREDICTION_HZ = 3
-STOP_FUTURE_FRAMES = 2
+STOP_FUTURE_FRAMES = 0 # if the model predict the future,  instead of the immediate thing
 SAFETY_SPEED = 10.0 # in m/s, cap the speed after time slow
 TIME_SLOW_FACTOR = 1.0
 
 KEYBOARD_TOPIC = "mkz_key_command"
 TWIST_COMM_TOPIC = "twist_predicted_results"
 
-# tunable parameters
-# parameters for the prior
-STRAIGHT_VALID_ANGLE = 20.0
-TURN_RIGHT_ZERO_THRES = 10.0
-TURN_LEFT_DISCOUNT_THRES = 5.0
-TURN_DISCOUNT = 0.3
-TYPE = "car_joint"
-
-global bridge, driving_model, vis_pub_full, prior_func, add_prior_to_logit
+global bridge, driving_model, vis_pub_full
 twist_comm_pub = rospy.Publisher(TWIST_COMM_TOPIC, Vector3, queue_size=1)
 
 def on_image_received(data):
@@ -41,15 +35,21 @@ def on_image_received(data):
     global bridge, driving_model, vis_pub_full
 
     img = bridge.imgmsg_to_cv2(data, "rgb8")
+
+    # TODO: compute the yaw, and speed from the driving model
+    yaw = 0.0
+    speed = 0.0
+    '''
     logits = driving_model.observe_a_frame(img)
     logits = logits[0]
     # we could not truncate the pdf distribution, otherwise the MAP would be quite unstable.
     #logits = add_prior_to_logit.prior_speed_limit(logits, TYPE, SAFETY_SPEED)
-    if prior_func is not None:
-        logits = prior_func(logits, TYPE)
+
     MAP = driving_model.continuous_MAP([logits])
     yaw, speed = MAP[0]
     yaw = -yaw
+    '''
+
     # apply the time slow factor
     speed /= TIME_SLOW_FACTOR
     yaw /= TIME_SLOW_FACTOR
@@ -72,34 +72,24 @@ def on_image_received(data):
     twist_comm_pub.publish(v3)
 
     # generate the visualization image
-    rendered = driving_model.generate_visualization(img, logits, "vis_continuous")
-    vis_pub_full.publish(bridge.cv2_to_imgmsg(rendered, "rgb8"))
+    # TODO: generate the visualization based on the driving model
+    #rendered = driving_model.generate_visualization(img, logits, "vis_continuous")
+    #vis_pub_full.publish(bridge.cv2_to_imgmsg(rendered, "rgb8"))
 
     print ("total time for on image receive is ", time.time()-time0)
     print "on image received"
 
 
 def on_key_received(data):
-    global prior_func
     key = data.data
     if key == "s":
         # set to no prior function
-        prior_func = None
         print("Prior: setting to no prior")
     elif key == "w":
-        prior_func = lambda logits, type: add_prior_to_logit.prior_straight(logits, type, STRAIGHT_VALID_ANGLE)
         print("Prior: setting to straight prior")
     elif key == "a":
-        prior_func = lambda logits, type: add_prior_to_logit.prior_left(logits, type,
-                                                                        TURN_RIGHT_ZERO_THRES,
-                                                                        TURN_LEFT_DISCOUNT_THRES,
-                                                                        TURN_DISCOUNT)
         print("Prior: setting to left prior")
     elif key == "d":
-        prior_func = lambda logits, type: add_prior_to_logit.prior_right(logits, type,
-                                                                         TURN_RIGHT_ZERO_THRES,
-                                                                         TURN_LEFT_DISCOUNT_THRES,
-                                                                         TURN_DISCOUNT)
         print("Prior: setting to right prior")
     else:
         print("invalid command", key)
@@ -111,44 +101,22 @@ def get_file_real_path():
 if __name__ == "__main__":
     rospy.init_node('BDD_Driving_Model')
     DRIVING_MODEL_PATH = sys.argv[1]
-    if len(sys.argv) > 2:
-        config_name = sys.argv[2]
-    else:
-        config_name = "invalid_config_name"
-
-    if len(sys.argv) > 3:
-        if sys.argv[3].lower() == "true":
-            is_lstm = True
-        elif sys.argv[3].lower() == "false":
-            is_lstm = False
-        else:
-            raise ValueError("is lstm param not valid")
-    else:
-        is_lstm = False
-
-    if len(sys.argv) > 4:
-        post_config_func = sys.argv[4]
-    else:
-        post_config_func = "common_config_MKZ"
 
     global PREDICTION_HZ, STOP_FUTURE_FRAMES
     if len(sys.argv) > 6:
         PREDICTION_HZ = int(sys.argv[5])
         STOP_FUTURE_FRAMES = int(sys.argv[6])
 
-    sys.path.append(os.path.join(os.path.dirname(get_file_real_path()), "../"))
-    global add_prior_to_logit
-    add_prior_to_logit = importlib.import_module("add_prior_to_logit")
-
     # a global shared data structure, list of [forward speed in m/s, yaw rate in rad/s]
-    global bridge, driving_model, vis_pub_full, prior_func
+    global bridge, driving_model, vis_pub_full
 
-    prior_func = None
     bridge = CvBridge()
 
     vis_pub_full = rospy.Publisher('vis_continuous_full', Image, queue_size=10)
 
     # BDD Driving model related
+    # TODO: add in the CIL model here
+    '''
     driving_model_code_path = os.path.join(os.path.dirname(get_file_real_path()), "../../BDD_Driving_Model")
     os.chdir(driving_model_code_path)
     sys.path.append(driving_model_code_path)
@@ -159,9 +127,9 @@ if __name__ == "__main__":
                             config_path=".",
                             is_lstm=is_lstm,
                             post_config_func=post_config_func)
+    '''
     rospy.Subscriber("temp_image_topic", Image, on_image_received, queue_size=1)
 
     rospy.Subscriber(KEYBOARD_TOPIC, String, on_key_received, queue_size=10)
 
     rospy.spin()
-
--- MKZ/run_model_lstm.sh
+++ MKZ/run_model_lstm.sh
@@ -3,14 +3,11 @@
 # when run on the VM, change 4 things: CACHE_BASE, private_key, use_fake_image, post_config_name
 
 MODEL_NAME="RFS_v7data_lstm" #"RFS_v7data_tcnn1_normalAug" #"RFS_v7data_lstm"
-IS_LSTM=false
 PREDICTION_HZ=3
 STOP_FUTURE_FRAMES=2
 
 CACHE_BASE="/home/bdd/yang/$MODEL_NAME/"
 PRIVATE_KEY="/home/bdd/yang/yang_private"
-#CACHE_BASE="/home/yang/Downloads/$MODEL_NAME/"
-#PRIVATE_KEY="/home/yang/.ssh/id_rsa"
 
 if ls $CACHE_BASE*bestmodel 1> /dev/null 2>&1; then
     echo "have already downloaded the model"
@@ -27,11 +24,9 @@ VIZ_OUTPUT_PATH=$CACHE_BASE"/"$dt".avi"
 
 roslaunch MKZ bdd.launch \
     driving_model_path:=$MODEL_ACTUAL_PATH \
-    config_name:=$MODEL_NAME \
-    is_lstm:=$IS_LSTM \
     use_real_image:=true \
     use_fake_image:=false \
     fake_tfrecord_path:=/home/yang/Desktop/real_car/vm_share/48.tfrecords \
-    viz_output_path:=$VIZ_OUTPUT_PATH \ #post_config_name:=common_config_ubuntuVM
+    viz_output_path:=$VIZ_OUTPUT_PATH \
     prediction_hz:=$PREDICTION_HZ \
     stop_future_frames:=$STOP_FUTURE_FRAMES
