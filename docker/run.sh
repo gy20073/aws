918a409697f776214411e2d5820f465a5b5a7f16
Yang Gao <gy20073@gmail.com>
2018-06-26 19:32:10 -0700

    before removing the cuda installation within docker

--- docker/run.sh
+++ docker/run.sh
@@ -4,7 +4,12 @@ docker run \
     --rm \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --runtime=nvidia \
-    -e NVIDIA_VISIBLE_DEVICES=2 \
+    --device /dev/nvidia1:/dev/nvidia0 \
+    --device /dev/nvidiactl:/dev/nvidiactl \
+    --device /dev/nvidia-uvm:/dev/nvidia-uvm \
+    --device /dev/nvidia-uvm-tools:/dev/nvidia-uvm-tools \
+    --device /dev/nvidia-modeset:/dev/nvidia-modeset \
+    -e NVIDIA_VISIBLE_DEVICES=7 \
     --name machine2 \
     --network tnet \
     gy20073/ros \
