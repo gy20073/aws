66f47688636986129009861ac53bb66611933bf4
Yang Gao <gy20073@gmail.com>
2018-07-07 21:35:35 -0700

    image view in docker and less clumsy run.sh file

--- docker/run.sh
+++ docker/run.sh
@@ -6,10 +6,7 @@ docker run \
     --volume="/scratch/yang/aws_data/:/root/mount:rw" \
     --runtime=nvidia \
     -e NVIDIA_VISIBLE_DEVICES="6,7" \
-    --name machine0 \
-    --network tnet \
     gy20073/ros \
     /bin/bash
 
-    # docker run -it --rm --net=host --name=yang_main --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES="0,1" -e ROS_MASTER_URI=$ROS_MASTER_URI -v "/:/root/mount:rw" -v "/dev:/dev:rw" gy20073/ros /bin/bash
-    #docker run -it --rm --net=host --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES="0,1" -e ROS_MASTER_URI=$ROS_MASTER_URI -v "/:/root/mount:rw" -v "/dev:/dev:rw" gy20073/ros /bin/bash
+    #docker run -it --rm --net=host --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES="0,1" -e ROS_MASTER_URI=$ROS_MASTER_URI -v "/:/root/mount:rw" gy20073/ros /bin/bash
