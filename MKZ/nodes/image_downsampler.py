9b95d6e22a80a15794c3d4c521533a3a7d4d3467
Yang Gao <gy20073@gmail.com>
2018-07-17 13:42:02 -0700

    polish image downsampler, fix minor bug in docker, create rosbag recording

--- MKZ/nodes/image_downsampler.py
+++ MKZ/nodes/image_downsampler.py
@@ -10,15 +10,20 @@ from cv_bridge import CvBridge
 import cv2
 
 image_pub = rospy.Publisher("/image_sender_0/H576W768", Image)
+image_pub_flipped = rospy.Publisher("/image_sender_0/flipped", Image)
 bridge = CvBridge()
 
 def on_image_received(data):
     img = bridge.imgmsg_to_cv2(data, "rgb8")
+    flipped = img[::-1, ::-1, :]
     img = cv2.resize(img,
                dsize=(768, 576),
                interpolation=cv2.INTER_AREA)
+    img = img[::-1, ::-1, :]
     img = bridge.cv2_to_imgmsg(img, "rgb8")
     image_pub.publish(img)
+
+    image_pub_flipped.publish(bridge.cv2_to_imgmsg(flipped, "rgb8"))
 
 if __name__ == "__main__":
     rospy.init_node('image_downsampler')
--- record_rosbag_and_video.bash
+++ record_rosbag_and_video.bash
@@ -1,8 +1,35 @@
 #!/usr/bin/env bash
 
-IMG_TOPIC="/vis_continuous_full"
-OUTPUT="/root/mount/store.avi"
+OUTPUT_PREFIX="/root/mount"
+# we are saving the images into a video
+ORIGINAL_IMAGE_TOPIC="/image_sender_0"
+OUTPUT_ORIGINAL_VIDEO=$OUTPUT_PREFIX"/original_images.avi"
+# all topics with downsampled images into a rosbag
+DOWNSAMPLED_IMAGE_TOPIC="/image_sender_0/H576W768"
+OUTPUT_BAG=$OUTPUT_PREFIX"/recording.bag"
+# also the visualization image with a video
+VIZ_IMAGE_TOPIC="/vis_continuous_full"
+OUTPUT_VIZ_VIDEO=$OUTPUT_PREFIX"/visualization_images.avi"
 
 # rosbag command goes here
+rosbag record \
+--split --duration 5m \
+-b 30720 \
+--output-name $OUTPUT_BAG \
+/nmea_sentence \
+/vehicle/brake_cmd /vehicle/brake_info_report /vehicle/brake_report \
+/vehicle/gear_report /vehicle/joint_states /vehicle/misc_1_report /vehicle/req_accel \
+/vehicle/gps/fix /vehicle/gps/time /vehicle/gps/vel \
+/vehicle/imu/data_raw \
+/vehicle/steering_cmd /vehicle/steering_report \
+/vehicle/surround_report /vehicle/suspension_report \
+/vehicle/throttle_cmd /vehicle/throttle_info_report /vehicle/throttle_report \
+/vehicle/tire_pressure_report /vehicle/turn_signal_cmd \
+/vehicle/wheel_position_report /vehicle/wheel_speed_report \
+/vehicle/twist /vehicle/twist_controller/parameter_descriptions /vehicle/twist_controller/parameter_updates \
+/xsens/fix /xsens/imu/data /xsens/imu/mag /xsens/imu_data_str /xsens/time_reference \
+$DOWNSAMPLED_IMAGE_TOPIC &
 
-rosrun image_view video_recorder image:=$IMG_TOPIC _filename:=$OUTPUT _codec:="X264" _encoding:="rgb8"
+rosrun image_view video_recorder image:=$ORIGINAL_IMAGE_TOPIC _filename:=$OUTPUT_ORIGINAL_VIDEO _codec:="X264" _encoding:="rgb8" &
+
+rosrun image_view video_recorder image:=$VIZ_IMAGE_TOPIC _filename:=$OUTPUT_VIZ_VIDEO _codec:="X264" _encoding:="rgb8" &
