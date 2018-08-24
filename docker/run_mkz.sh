28c24dac55c7c73b54dab7b429395357ea58a69b
Yang Gao <gy20073@gmail.com>
2018-08-24 12:46:40 -0700

    update CIL launch and bash files, add mkz docker running file

---
+++ docker/run_mkz.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+docker run \
+    -it \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="/:/root/mount/:rw" \
+    --volume="/home/bdd/intel/aws:/data/yang/code/aws:rw" \
+    --runtime=nvidia \
+    -e NVIDIA_VISIBLE_DEVICES="0,1" \
+    -e ROS_MASTER_URI=$ROS_MASTER_URI \
+    --net=host \
+    gy20073/ros \
+    /bin/bash
