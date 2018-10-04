7af908b93f2015875211dde1d69ea4a4f427f8d1
Yang Gao <gy20073@gmail.com>
2018-10-04 15:40:51 -0700

    better convert gta script

--- parse_dagger_12_convert.sh
+++ parse_dagger_12_convert.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-input_path="/Volumes/yang/daggeryang"
-output_path="/Volumes/Data/Downloads/vladlen/data/gta_batch2"
+input_path="/Volumes/conditionR6/dagger_yang"
+output_path="/Volumes/Data/Downloads/vladlen/data/gta_batch3"
 num_threads="4"
 
 python3 gta_tools/hybrid_client/parse_dagger.py $input_path
@@ -10,6 +10,6 @@ python3 gta_tools/hybrid_client/parse_dagger02.py $input_path
 declare -a arr=("val" "train")
 for set in "${arr[@]}"
 do
-    python2 CIL_modular/convert_dqwang_to_h5.py $input_path $output_path+"/"+$set $set $num_threads
+    python2 CIL_modular/convert_dqwang_to_h5.py $input_path $output_path"/"$set $set $num_threads
 done
 
