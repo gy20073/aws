9ee5aa6508581be7e66dc14eccc041f9875f6f4f
Yang Gao <gy20073@gmail.com>
2019-06-26 13:55:44 +0800

    allow train within a docker

--- docker/run_train.sh
+++ docker/run_train.sh
@@ -1,13 +1,37 @@
 #!/usr/bin/env bash
+
+CODE_PATH="/home/yang/code/aws"
+DATA="/home/yang/data/aws_data2/"
+mount_dataset_base="/home/yang/data/aws_data/carla_collect/"
+mount_dataset_id1="exptown_v9_noise75_way"
+# end of path specification
+
+# make dirs
+mkdir $DATA
+mkdir $DATA"carla_collect"
+mkdir $DATA"CIL_modular_data"
+mkdir $DATA"CIL_modular_data/benchmark_all"
+mkdir $DATA"CIL_modular_data/_benchmarks_results"
+mkdir $DATA"CIL_modular_data/models"
+mkdir $DATA"CIL_modular_data/results"
+
+PORT=$DATA"CIL_modular_data/port.txt"
+if [ ! -f $PORT ]; then
+    echo "3000" > $PORT
+fi
+
 docker run \
     -it \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="/scratch/yang/:/scratch/yang/:rw" \
-    --volume="/data2/yang/code/aws:/data/yang/code/aws:rw" \
     --runtime=nvidia \
     -e NVIDIA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
     --net=host \
+    --volume=$CODE_PATH":/data/yang/code/aws:rw" \
+    --volume=$DATA":/home/yang/data/aws_data:rw" \
+    --volume=$DATA":/scratch/yang/aws_data:rw" \
+    --volume=$mount_dataset_base$mount_dataset_id1":/home/yang/data/aws_data/carla_collect/"$mount_dataset_id1":rw" \
+    --volume=$mount_dataset_base$mount_dataset_id1":/scratch/yang/aws_data/carla_collect/"$mount_dataset_id1":rw" \
     gy20073/ros \
     /bin/bash
 
-    #docker run -it --rm --net=host --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES="0,1" -e ROS_MASTER_URI=$ROS_MASTER_URI -v "/:/root/mount:rw" gy20073/ros /bin/bash
+    #--volume="/scratch/yang/:/scratch/yang/:rw" \
+    #
