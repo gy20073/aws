6f48091c6fec02a315ed06b12c984019e663037b
Yang Gao <gy20073@gmail.com>
2019-06-26 15:22:53 +0800

    add a sample to do on taniwha

--- docker/run_train.sh
+++ docker/run_train.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-CODE_PATH="/home/yang/code/aws"
+# begin of path specifications
+CODE_PATH="/home/yang/aws"
 DATA="/home/yang/data/aws_data2/"
-mount_dataset_base="/home/yang/data/aws_data/carla_collect/"
-mount_dataset_id1="exptown_v9_noise75_way"
+mount_dataset_base="/shared/yang/data1/aws_data/carla_collect"
+mount_dataset_id1="steer103_v5_way_v2_town02"
 # end of path specification
 
 # make dirs
-mkdir $DATA
+mkdir -p $DATA
 mkdir $DATA"carla_collect"
 mkdir $DATA"CIL_modular_data"
 mkdir $DATA"CIL_modular_data/benchmark_all"
@@ -24,7 +25,6 @@ docker run \
     -it \
     --runtime=nvidia \
     -e NVIDIA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
-    --net=host \
     --volume=$CODE_PATH":/data/yang/code/aws:rw" \
     --volume=$DATA":/home/yang/data/aws_data:rw" \
     --volume=$DATA":/scratch/yang/aws_data:rw" \
